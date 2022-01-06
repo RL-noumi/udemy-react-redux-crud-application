1e164c359b99493322641fbf1137489e0a440494
RL-noumi <noumi@realink-group.co.jp>
2022-01-06 01:09:33 +0000

    components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
+// class App extends Component {
+//   render() {
 
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I,m clicked.")}} />
-      </React.Fragment>
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I,m clicked.")}} />
+//       </React.Fragment>
 
-    );
+//     );
     
   
-  }
+//   }
+// }
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+const Cat = () => {
+  return <div>Meao!</div>
 }
 
 export default App;
