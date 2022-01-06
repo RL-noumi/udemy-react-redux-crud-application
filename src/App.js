6578e865200a28fd91b66c469c2d2bbd028815e8
RL-noumi <noumi@realink-group.co.jp>
2022-01-06 00:32:03 +0000

    react fragment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello world!!!!!!!!!!!!!</h1>
+
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I,m clicked.")}} />
+      </React.Fragment>
+
+    );
+    
+  
   }
 }
 
